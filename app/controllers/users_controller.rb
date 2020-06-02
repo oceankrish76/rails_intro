f4ccceb05bf66639ac3b092d5c96cafd7b692a3e
oceankrish76 <oceankrish76@gmail.com>
2020-06-02 17:12:55 +0300

    bootstrap + author on UI post

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -73,6 +73,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:fname, :lname, :location, :created_at)
+      params.require(:user).permit(:fname, :lname, :location, :created_at, :id)
     end
 end
